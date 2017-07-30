83da3cb009b295438effb93f0ad43466bed6d37e
Serhii Savchenko <w1nner1715@gmail.com>
2017-07-30 20:16:08 +0300

    Add files via upload

---
+++ QueensProblem.java
@@ -0,0 +1,64 @@
+package different;
+
+public class QueensProblem {
+
+	static int N = 8;
+	static int[] chessboard = new int [N];
+	static int index = 0;
+	static int version = 0;
+	
+	public static void main(String[] args){
+
+		while (chessboard[0]<N) {							// для всех столбцов поля
+			
+			if (checked()){									// если на данный момент нет ферзей под боем
+				
+				if (index == (N-1)) {						// если мы уже заполнили все строки
+					System.out.print(++version + ": ");
+					printResult(chessboard);
+					chessboard[index]++;
+				}
+				else {										// если рассмотрели еще не все строки 
+					index++;								// переходим на следующую
+				}
+				
+			}
+			else {											// проверка не пройдена => сдвигаем ферзя на текущей позиции вверх
+				chessboard[index]++;
+			}
+			
+		}
+	
+	}
+
+	static boolean checked() {
+
+		if (index == 0) {
+			return true;
+		}
+
+		if (chessboard[index]>(N-1)){
+			chessboard[index] = 0;
+			index--;
+			return false;
+		}
+
+		for (int i=0; i<index; i++){
+			if ((chessboard[index] == chessboard[i]) || ((Math.abs(chessboard[index] - chessboard[i])) == (index-i))){
+				return false;
+			}
+		}
+
+		return true;
+	}
+	
+	///////////////////////////////////
+	private static void printResult(int[] tempChessboard) {
+		int j;
+		for (j = 0; j < (tempChessboard.length-1); j++){
+			System.out.print("["+j+"]="+tempChessboard[j]+" ");
+		}
+		System.out.println("["+j+"]="+tempChessboard[j]+" ");
+	}
+	
+}
